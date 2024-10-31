ae9f28b36053611e2c2f38bd59f97583f15226c7
Marco <127329894+Makoy0718@users.noreply.github.com>
2024-10-30 19:00:26 -0600

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,16 @@
+const mobilebar = document.getElementById("popup");
+
+function showTab() {
+  if (mobilebar.classList.contains("active")) {
+    mobilebar.classList.remove("active");
+  } else {
+    mobilebar.classList.add("active");
+  }
+}
+
+function navigate(path) {
+  window.location = path;
+}
+
+    
+
